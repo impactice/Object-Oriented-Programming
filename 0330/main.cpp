48459c56befbdfa24947a639feb9094f4ce3a9d0
impactice <107308111+impactice@users.noreply.github.com>
2026-03-30 11:49:21 +0900

    Add files via upload

--- 0330/main.cpp
+++ 0330/main.cpp
@@ -18,12 +18,29 @@ Circle getCircle() { //객체 리턴
     return tmp; //tmp의 복사본이 반환
 }
 
+
+//TODO: readRadius() 함수를 작성하세요 
+// - 매개변수: Circle에 대한 참조 
+//- 동작: cin으로 정수를 입력받아 c.setRadius() 호출
+
+void ReadRadius(Circle &c) {
+    int r;
+    cout<<"반지름 입력 : ";
+    cin >> r;
+    c.setRadius(r);
+
+}
+
 int main() {
     SetConsoleCP(CP_UTF8);        // 입력도 UTF-8
     SetConsoleOutputCP(CP_UTF8);  // 출력 UTF-8
-    Circle c;
-    c = getCircle();
-    cout<<c.getArea()<<endl;
+    //Circle c;
+    //c = getCircle();
+    Circle donut;
+    ReadRadius(donut);
+    cout << "donut의 면적 = "<<donut.getArea() << endl;
+    //cout<<c.getArea()<<endl;
+    
     //Circle waffle(30);
     //increase(waffle);
     //increase_addr(&waffle);
--- 0330/swap.cpp
+++ 0330/swap.cpp
@@ -12,12 +12,19 @@ void swap_addr(int *a, int *b) {
     *b = temp;
 }
 
+void swap_ref(int &a, int& b){
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+
 int main() {
     SetConsoleCP(CP_UTF8);        // 입력도 UTF-8
     SetConsoleOutputCP(CP_UTF8);  // 출력 UTF-8
     int m = 2, n = 3;
     std::cout<<"바꾸기 전: m = " <<m<<", n = "<<n<<std::endl;
     //swap(m,n);
-    swap_addr(&m, &n);
+    //swap_addr(&m, &n);
+    swap_ref(m,n);
     std::cout<<"바꾼 후 : m = "<<m<<", n =" <<n<<std::endl;
 }
