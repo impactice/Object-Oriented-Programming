6f31b8330ce360b1a2ea000464964564fa4cbf79
impactice <107308111+impactice@users.noreply.github.com>
2026-05-11 09:27:35 +0900

    Add files via upload

---
+++ 0309/coutcin.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream> 
+//#include <windows.h> 
+
+int main() {
+    //system("chcp 65001");
+    std::cout << "이름을 입력하세요: ";
+
+    std::string name;
+    std::cin >> name;
+
+    std::cout << "안녕하세요, " << name << "님!" << std::endl;
+
+    return 0;
+}
