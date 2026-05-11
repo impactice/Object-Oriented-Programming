70be033487d84f9735ffbaacccdde52087e714c2
impactice <107308111+impactice@users.noreply.github.com>
2026-05-11 09:28:44 +0900

    Add files via upload

--- 0323/main.cpp
+++ 0323/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <windows.h>
 #include "Circle.h"
 using namespace std;
 
 int main(){
+    SetConsoleCP(CP_UTF8);        // 입력도 UTF-8
+    SetConsoleOutputCP(CP_UTF8);  // 출력 UTF-8
     Circle circleArray[3]={Circle(10),Circle(20),Circle(30)}; //Circle 클래스의 객체 배열 선언
+    //인덱스로 접근
+    for(int i=0; i< 3; i++) {
+        cout<<"Circle "<<i+1<<"의 면적은" <<circleArray[i].getArea()<<endl; // 멤버 함수 호출
+    }
+    // 포인터로 접근 
+    Circle *p = circleArray; // 배열 이름 = 배열의 첫 번째 요소의 주소
+    for(int i=0; i< 3; i++) {
+        cout<<"Circle "<<i+1<<"의 면적은" <<p->getArea()<<endl; // 멤버 함수 호출
+    }
     //Circle circleArray[3](56); // 이거는 당연히 안됨
     /*
     Circle pizza(20), donut; //Circle 클래스의 객체 pizza와 donut 선언
