83269231312c42cb45b585527c65fd5759d08889
impactice <107308111+impactice@users.noreply.github.com>
2026-05-11 11:45:08 +0900

    Add files via upload

--- 0511/vectorTest.cpp
+++ 0511/vectorTest.cpp
@@ -4,17 +4,24 @@ using namespace std;
 
 int main(){
     vector<int> v;
-    vector<int>::iterator it;
 
     v.push_back(10);
     v.push_back(20);
     v.push_back(30);
 
-    //iteractor ~
+    for(auto it=v.begin(); it!=v.end(); it++){ //vector<int>::iterator it; 이걸 안 쓰고 auto로 쓸 수 있음
+        cout << *it << " ";
+    }
+    cout <<endl;
+
+    //iteractor를 사용한 순회
+    //vector<int>::iterator it;
+    /*
     for(it=v.begin(); it!=v.end(); it++){
         cout << *it << " ";
     }
     cout <<endl;
+    */
 
     /*
     for(int i=0; i<v.size(); i++){
@@ -26,8 +33,16 @@ int main(){
     v[0] =100;
     int n = v[2];
     v.at(2) = 5;
-     for(int i=0; i<v.size(); i++){
+
+    /*
+    for(int i=0; i<v.size(); i++){
         cout << v[i] << " ";
     }
     cout <<endl;
+    */
+
+    //범위 기반 for문 사용가능
+    for(auto n : v){
+        cout << n << " ";
+    }
 }
